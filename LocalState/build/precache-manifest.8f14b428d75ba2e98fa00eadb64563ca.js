4a65a7b7c086b761359a277c04c3797b9070209d
Rie Kato <katoriexxxkatorie@gmail.com>
2020-03-11 05:58:15 +0900

    Add LocalState

---
+++ LocalState/build/precache-manifest.8f14b428d75ba2e98fa00eadb64563ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585ece340aa3e44bdf6768f12c5eefe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf0f330d995380c9634",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "837b749e9158fcfcd11e",
+    "url": "/static/js/2.50f51ea0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.50f51ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf0f330d995380c9634",
+    "url": "/static/js/main.0a7a9dfa.chunk.js"
+  },
+  {
+    "revision": "c7907c5a0ab55172975d",
+    "url": "/static/js/runtime-main.fdd3cfbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
